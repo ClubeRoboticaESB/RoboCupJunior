597b003b7f6b954dd3b81310fec8527dc590aa39
danielalexis <danielalexis.o.pereira@gmail.com>
2022-04-02 01:08:29 +0100

    change pins and enviroment to arduino mega

--- 2022/Firmware/include/pins.h
+++ 2022/Firmware/include/pins.h
@@ -1,36 +1,36 @@
 // RGB Color Sensors
 // Sensor 1
-#define CS1_S0 PA15
-#define CS1_S1 PB3
-#define CS1_S2 PB4
-#define CS1_S3 PB5
-#define CS1_OUTPUT PB12
+#define CS1_S0 23
+#define CS1_S1 24
+#define CS1_S2 25
+#define CS1_S3 26
+#define CS1_OUTPUT 22
 
 // Sensor 2
-#define CS2_S0 PB14
-#define CS2_S1 PB15
-#define CS2_S2 PA8
-#define CS2_S3 PA9
-#define CS2_OUTPUT PB13
+#define CS2_S0 28
+#define CS2_S1 29
+#define CS2_S2 30
+#define CS2_S3 31
+#define CS2_OUTPUT 27
 
 
 // Motor Drivers
 // Driver 1
-#define IN1A PB1
-#define IN2A PB0
-#define IN3A PA7
-#define IN4A PA6
+#define IN1A 2
+#define IN2A 3
+#define IN3A 4
+#define IN4A 5
 
 // Driver 2
-#define IN1B PA10
-#define IN2B PA5
-#define IN3B PB8
-#define IN4B PB9
+#define IN1B 6
+#define IN2B 7
+#define IN3B 8
+#define IN4B 9  
 
 
 // IR Sensors (Line Sensor)
-#define IR1 PA4
-#define IR2 PA3
-#define IR3 PA2
-#define IR4 PA1
-#define IR5 PA0
+#define IR1 A0
+#define IR2 A1
+#define IR3 A2
+#define IR4 A3
+#define IR5 A4
