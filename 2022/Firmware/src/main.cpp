77850cdaefa2afe47943733805a7c9f0cb522956
danielalexis <danielalexis.o.pereira@gmail.com>
2022-04-10 15:30:43 +0100

    Line Follower Implementation and Turning using sensors started

--- 2022/Firmware/src/main.cpp
+++ 2022/Firmware/src/main.cpp
@@ -8,8 +8,8 @@
 tcs3200 rgbSensor1(CS1_S0,CS1_S1,CS1_S2,CS1_S3,CS1_OUTPUT);
 tcs3200 rgbSensor2(CS2_S0,CS2_S1,CS2_S2,CS2_S3,CS2_OUTPUT);
 
-MDriver driver(IN1A,IN2A,IN3A,IN4A);
-MDriver driver(IN1B,IN2B,IN3B,IN4B);
+MDriver driverFront(IN1A,IN2A,IN3A,IN4A);
+MDriver driverBack(IN1B,IN2B,IN3B,IN4B);
 
 DistSense distSensor1(DIST_TRIG1, DIST_ECHO1);
 DistSense distSensor2(DIST_TRIG2, DIST_ECHO2);
@@ -18,26 +18,54 @@ LineSense line(IR1, IR2, IR3, IR4, IR5);
 
 int line_calib, green1_calib, green2_calib;
 
+int state = 0;
+int speed = 100;
 void setup() {
     while (digitalRead(PIN_BUTTON) != HIGH) {
-        Serial.println("Click to start calibration");
+        Serial.println("Click to start");
     }
-    do {
-        Serial.println("Click to calibrate the line");
-        line_calib = line.readIR3();
-        Serial.println("Line Calibration: " + line_calib);
-    } while (!line_calib);
-    do {
-        Serial.println("Click to calibrate the RGB Sensors");
-        green1_calib = rgbSensor1.read_green();
-        green2_calib = rgbSensor2.read_green();
-        Serial.println("Green 1 Calibration: " + green1_calib);
-        Serial.println("Green 2 Calibration: " + green1_calib);
-    } while (!green1_calib && !green2_calib);
 
-    
 }
 
 void loop() {
+   /*
+   * 0 - Line Follow (Green Squares not detected)
+   * 1 - Turn (RGB Sensors)
+   * 2 - Go Around Obstacle
+   * 3 - Victims
+   */
+   switch (state) {
+       case 0:
+            // First we detect if all of the sensor is on a white space
+            // this is so the robot go foward on gaps
+            if (line.readIR1() < line_calib && line.readIR2() < line_calib && line.readIR3() < line_calib && line.readIR4() < line_calib &&line.readIR5() < line_calib) {
+                driverFront.move(speed, speed);
+                driverBack.move(speed, speed);
+            }
 
+            // Skip this cycle if a green square was detected
+            if (rgbSensor1.read_green() > green1_calib || rgbSensor2.read_green() > green2_calib) {
+                state = 1;
+                break; // Dont continue the cycle
+            } 
+
+            // Maintain robot between 1500 - 2500
+            if (line.weighted_average() > 1500 ) {
+                driverFront.move(speed, speed-20);
+                driverBack.move(speed, speed-20);
+            } else if (line.weighted_average() < 2500 ) {
+                driverFront.move(speed-20, speed);
+                driverBack.move(speed-20, speed);                
+            }
+            break;
+        case 2:
+            if (rgbSensor1.read_green() > green1_calib && rgbSensor2.read_green() > green2_calib) {
+                driverFront.move(speed, speed-20);
+                driverBack.move(speed, speed-20);
+                delay(100);
+            
+            }
+            
+
+   }
 }
